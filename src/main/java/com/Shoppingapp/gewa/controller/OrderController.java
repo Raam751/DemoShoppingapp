cfecd7ca28d8b7b77df0508edcaeda5f0af55a12
Raam <ydt751@gmail.com>
2025-12-27 18:54:25 +0530

    Enable global CORS for production deployment

--- src/main/java/com/Shoppingapp/gewa/controller/OrderController.java
+++ src/main/java/com/Shoppingapp/gewa/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.Shoppingapp.gewa.controller;
 
-
 import com.Shoppingapp.gewa.model.dto.OrderRequest;
 import com.Shoppingapp.gewa.model.dto.OrderResponse;
 import com.Shoppingapp.gewa.service.OrderService;
@@ -9,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class OrderController {
@@ -18,13 +17,13 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping("/orders/place")
-    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest){
+    public ResponseEntity<OrderResponse> placeOrder(@RequestBody OrderRequest orderRequest) {
         OrderResponse orderResponse = orderService.placeOrder(orderRequest);
         return new ResponseEntity<>(orderResponse, HttpStatus.CREATED);
     }
 
     @GetMapping("/orders")
-    public ResponseEntity<java.util.List<OrderResponse>> getAllOrders(){
+    public ResponseEntity<java.util.List<OrderResponse>> getAllOrders() {
         java.util.List<OrderResponse> orders = orderService.getAllOrderResponses();
         return new ResponseEntity<>(orders, HttpStatus.ACCEPTED);
     }
--- src/main/java/com/Shoppingapp/gewa/controller/ProductController.java
+++ src/main/java/com/Shoppingapp/gewa/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class ProductController {
